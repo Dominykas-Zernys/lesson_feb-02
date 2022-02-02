16b2c4d7a5ec59de65b70e471d55449897319f6b
Dominykas-Zernys <zernysdominykas@gmail.com>
2022-02-02 13:57:53 +0200

    server setup

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-console.log('serveris');
+// EXPRESS AND CORS
+
+const express = require('express')
+const cors = require('cors')
+const { response } = require('express')
+
+const app = express()
+const PORT = 3000
+
+// MIDDLEWARE
+
+// ROUTES
+
+app.get('/test', (req, res) => {
+    res.send('<h2>Back-end on line</h2>')
+})
+
+// SERVER
+app.listen(PORT, console.log(`server is running on port ${PORT}`))
